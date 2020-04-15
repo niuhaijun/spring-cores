a06f0f9d752aafa9028e6677a62171c4424a8a03
牛海军 <niuhaijun@100tal.com>
2020-04-15 20:51:51 +0800

    解决循环引用值三级缓存

---
+++ spring-cycle-reference/src/main/java/com/niu/cycle/A.java
@@ -0,0 +1,22 @@
+package com.niu.cycle;
+
+/**
+ * @author niuhaijun
+ * @date 2020-04-15 20:44
+ * @version 1.0
+ * @description: xxx
+ */
+public class A {
+
+	private B b;
+
+	public B getB() {
+
+		return b;
+	}
+
+	public void setB(B b) {
+
+		this.b = b;
+	}
+}
---
+++ spring-cycle-reference/src/main/java/com/niu/cycle/B.java
@@ -0,0 +1,22 @@
+package com.niu.cycle;
+
+/**
+ * @author niuhaijun
+ * @date 2020-04-15 20:45
+ * @version 1.0
+ * @description: xxx
+ */
+public class B {
+
+	private A a;
+
+	public A getA() {
+
+		return a;
+	}
+
+	public void setA(A a) {
+
+		this.a = a;
+	}
+}
