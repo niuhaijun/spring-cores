b8c7e7f4858fdae0e20ab1802ba6dc07d88cede3
牛海军 <niuhaijun@100tal.com>
2020-04-15 21:00:30 +0800

    循环依赖解决方案

--- spring-cycle-reference/src/test/java/com/niu/springcyclereference/SpringCycleReferenceApplicationTests.java
+++ spring-cycle-reference/src/test/java/com/niu/springcyclereference/SpringCycleReferenceApplicationTests.java
@@ -1,12 +1,17 @@
 package com.niu.springcyclereference;
 
+import com.niu.springcyclereference.service.HuangService;
 import com.niu.springcyclereference.service.NiuService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 public class SpringCycleReferenceApplicationTests {
+
+	@Autowired
+	private ApplicationContext context;
 
 	@Autowired
 	private NiuService niuService;
@@ -16,6 +21,13 @@ public class SpringCycleReferenceApplicationTests {
 
 		niuService.hello();
 		niuService.getHuangService().hello();
+	}
+
+	@Test
+	public void getBean() {
+
+		context.getBean(NiuService.class);
+		context.getBean(HuangService.class);
 	}
 
 }
